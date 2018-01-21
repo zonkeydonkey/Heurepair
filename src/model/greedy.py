ff42177bb5f6a0c8574ef5e72434c60c2b00e03e
Iza <izuu13@gmail.com>
2018-01-21 05:30:51 +0100

    few changes

--- src/model/greedy.py
+++ src/model/greedy.py
@@ -1,13 +1,15 @@
 import sys
-from model import roads_list, max_road_demand
 from roads_map import *
 import copy
 
 def metrics(map_custom, map_original):
     lenMap = len(map_custom['roads'])
     difference = 0
+    if (len(map_custom['cities'])!=len(map_original['cities'])) or (len(map_custom['roads'])!=len(map_original['roads'])):
+        print("Cant compare - different number of cities or roads")
     for i in range(lenMap):
-        # tutaj powinien być warunek sprawdzający czy porównujemy drogi do tych samych/z tych samych miast
+        if map_custom['roads'][i]['city_a'] != map_original['roads'][i]['city_a'] or map_custom['roads'][i]['city_b'] != map_original['roads'][i]['city_b'] or map_custom['roads'][i]['city_a'] != map_original['roads'][i]['city_a']:
+            print("Can't compare - different roads on same position")
         difference += (map_custom['roads'][i]['capacity'] - map_original['roads'][i]['capacity'])*map_original['roads'][i]['length']
     return difference
 
@@ -16,7 +18,7 @@ def greedy(map_original):
     map_copy = copy.deepcopy(map_original)
     lengths = map_copy['roads']
     lengths = sorted(lengths, key=lambda k: k['length'])
-    cur_expenses,curr_espenses=0,0
+    cur_expenses=0
 
     for i in range(len(lengths)):
         city_A=lengths[i]['city_a']
@@ -29,16 +31,17 @@ def greedy(map_original):
         cap_of_city_A=city_A_data['demand']['from']
         cap_of_city_B=city_B_data['demand']['to']
 
-        cap_of_city=min(cap_of_city_A, cap_of_city_B)
-        lengths[i]['cap']=cap_of_city
+        cap_between_cities=min(cap_of_city_A, cap_of_city_B)
+        lengths[i]['cap']=cap_between_cities
 
-        if cur_expenses<budget:
-            curr_expenses=cur_expenses
-            price=(cap_of_city-map_original['roads'][i]['capacity'])*lengths[i]['length']
-            cur_expenses+=price
-
-    print(curr_espenses)
-    print(budget)
+        if cur_expenses<budget and map_original['roads'][i]['capacity'] < cap_between_cities:
+            if (cap_between_cities > map_original['roads'][i]['capacity']):
+                newCapacity = (cap_between_cities - map_original['roads'][i]['capacity'])
+                price= newCapacity * lengths[i]['length']
+                print("Debug: before - ", lengths[i]['capacity'])
+                lengths[i]['capacity'] = newCapacity
+                cur_expenses+=price
+                print("Debug: after - ",lengths[i]['capacity'])
     return map_copy
 
 repaired_city = greedy(roads_map)
