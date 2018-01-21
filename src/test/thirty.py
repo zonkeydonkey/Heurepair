fd8943eaf7893ed63744935343511b6b93c216dc
Iza <izuu13@gmail.com>
2018-01-21 15:47:59 +0100

    few changes

--- src/test/thirty.py
+++ src/test/thirty.py
@@ -71,6 +71,30 @@ roads_map = {
                 'to': 189.3
             }
         }
+
+        {
+            'name': "gdansk",
+            'demand': {
+                'from': 156.3,
+                'to': 99.4
+            }
+        }
+
+        {
+            'name': "olsztyn",
+            'demand': {
+                'from': 43.2,
+                'to': 44.3
+            }
+        }
+
+        {
+            'name': "ciechanow",
+            'demand': {
+                'from': 39.5,
+                'to': 29.7
+            }
+        }
     ],
 
     'roads': [
@@ -115,35 +139,35 @@ roads_map = {
             'capacity': 54.9,
             'length': 76.3
         },
-        
+
         {
             'city_a': "katowice",
             'city_b': "ketrzyn",
             'capacity': 17.6,
             'length': 37.2
         },
-        
+
         {
             'city_a': "kielce",
             'city_b': "ketrzyn",
             'capacity': 42.8,
             'length': 62.4
         },
-        
+
         {
             'city_a': "krakow",
             'city_b': "ketrzyn",
             'capacity': 9.54,
             'length': 37.2
         },
-        
+
         {
             'city_a': "katowice",
             'city_b': "koluszki",
             'capacity': 52.1,
             'length': 39.7
         },
-
+# 10
         {
             'city_a': "kielce",
             'city_b': "warszawa",
@@ -185,34 +209,106 @@ roads_map = {
             'capacity': 83.4,
             'length': 14.3
         },
-        
+
         {
             'city_a': "szczecin",
             'city_b': "ketrzyn",
             'capacity': 68.3,
             'length': 37.2
         },
-        
+
         {
             'city_a': "kielce",
             'city_b': "szczecin",
             'capacity': 7.34,
             'length': 8.45
         },
-        
+
         {
             'city_a': "szczecin",
             'city_b': "sucha",
             'capacity': 92.3,
             'length': 45.2
         },
-        
+
         {
             'city_a': "katowice",
             'city_b': "sucha",
             'capacity': 17.4,
             'length': 43.8
+        },
+
+# 20
+        {
+            'city_a': "kielce",
+            'city_b': "ciechanow",
+            'capacity': 17.9,
+            'length': 40.2
+        },
+
+        {
+            'city_a': "gdansk",
+            'city_b': "olsztyn",
+            'capacity': 30.7,
+            'length': 24.1
+        },
+
+        {
+            'city_a': "olsztyn",
+            'city_b': "ciechanow",
+            'capacity': 18.9,
+            'length': 20.2
+        },
+
+        {
+            'city_a': "ciechanow",
+            'city_b': "koluszki",
+            'capacity': 12.7,
+            'length': 70.2
+        },
+
+        {
+            'city_a': "koluszki",
+            'city_b': "olsztyn",
+            'capacity': 37.8,
+            'length': 102.5
+        },
+
+        {
+            'city_a': "gdansk",
+            'city_b': "koluszki",
+            'capacity': 36.9,
+            'length': 45.9
+        },
+
+        {
+            'city_a': "sucha",
+            'city_b': "ciechanow",
+            'capacity': 26.9,
+            'length': 59.3
+        },
+
+        {
+            'city_a': "olsztyn",
+            'city_b': "sucha",
+            'capacity': 90.9,
+            'length': 10.2
+        },
+
+        {
+            'city_a': "kielce",
+            'city_b': "gdansk",
+            'capacity': 99.9,
+            'length': 90.2
+        },
+
+        {
+            'city_a': "ketrzyn",
+            'city_b': "gdansk",
+            'capacity': 12.1,
+            'length': 16.8
         }
+# 30
     ]
 }
 
